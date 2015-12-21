dcde782125fb15bb0c4694bdd155806fe7bc1fec
Sumeet Jain <sumeetjain@Sumeets-MacBook-Air.local>
2015-12-20 22:37:50 -0500

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var twitterAPI = require('node-twitter-api')
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +13,32 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/authorized', function(request, response) {
+  response.send('token: ' + request.query.oauth_token + ' verifier:' + request.query.oauth_verifier); 
+});
+
+app.get('/twitter', function(request, response) {
+  var twitter = new twitterAPI({
+	consumerKey: 'xaOgQc0Im1PooxmkCJPuoQ',
+	consumerSecret: '8R3AzD6IaiJ1UUfPLihtV80T0nJ8vMh1CPDIxDCSU',
+	callback: 'https://twitter-guess.herokuapp.com/authorized'
+  });
+  var token = '';
+  var tokenSecret = '';
+  twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(requestToken);
+		token = requestToken;
+		tokenSecret = requestTokenSecret;
+		response.redirect(twitter.getAuthUrl(requestToken));
+		//store token and tokenSecret somewhere, you'll need them later; redirect user 
+	}
+  });
+  //response.send(cool());
 });
 
 app.get('/cool', function(request, response) {
