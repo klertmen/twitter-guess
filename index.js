7cbb311ad3a9ba79a37c3a9af1d0229eb14ace32
Sumeet <teemus@gmail.com>
2015-12-22 10:00:40 -0500

    Fix subset

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/game', function(request, response) {
 			  request.session.users = users;
 			  redisClient.set(randomTweet.id, randomTweet.user.id);
 			  populateRedisWithTweets(requestToken, data);
-			  #TODO: fix the subset
+			  // TODO: fix the subset
 			  var subsetUsers = _.slice(users, end=10);
 			  subsetUsers.push(_.find(users, 'id', randomTweet.user.id));
 			  response.render('pages/twitter', { users: subsetUsers, tweet: randomTweet.text, tweetId: randomTweet.id });
